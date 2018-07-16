ddff1bf6a6b5bdb5fb2cb6a6ba73206b403fd87c
GitRubyManhPhuc <thachnguyenhl@gmail.com>
2018-07-16 14:51:06 +0700

    Chapter8: Basic Login

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,15 @@ class User < ApplicationRecord
   validates :password, presence: true, length:
     {minimum: Settings.minimum.pass_min}
 
+  def self.digest string
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create string, cost: cost
+  end
+
   private
 
   def downcase_email
