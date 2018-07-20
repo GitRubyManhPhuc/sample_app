34f88ac1d29bbc2afd35c6b3c256c8edda81918f
GitRubyManhPhuc <thachnguyenhl@gmail.com>
2018-07-20 11:27:14 +0700

    chapter 11 Account acrivation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
   before_save :downcase_email
+  before_create :create_activation_digest
   validates :name, presence: true, length:
     {maximum: Settings.maximum.name_max}
 
@@ -10,7 +11,7 @@ class User < ApplicationRecord
     {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   has_secure_password
 
-  validates :password, presence: true, length:
+  validates :password, presence: true, allow_nil: true, length:
     {minimum: Settings.minimum.pass_min}
 
   def forget
@@ -34,17 +35,31 @@ class User < ApplicationRecord
 
   def remember
     self.remember_token = User.new_token
-    update_attribute :remember_digest, User.digest(remember_token)
+    update_attributes remember_digest: User.digest(remember_token)
   end
 
-  def authenticated? remember_token
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def activate
+    update_columns activated: true, activated_at: Time.zone.now
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  def authenticated? attribute, token
+    digest = send "#{attribute}_digest"
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password? token
   end
 
   private
 
   def downcase_email
     email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
